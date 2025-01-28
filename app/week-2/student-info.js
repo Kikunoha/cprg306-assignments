4f8c4c868886947d1810d7ae0165c5b1cd72050a
Finnley Cottam <finnley.cottam@edu.sait.ca>
2025-01-27 21:06:45 -0700

    Week 2 Assignment Commit

--- app/week-2/student-info.js
+++ app/week-2/student-info.js
@@ -3,6 +3,7 @@ export default function StudentInfo() {
     return (
       <main>
         <p>Finnley Cottam</p>
+        <p> <Link href="https://github.com/Kikunoha/cprg306-assignments">https://github.com/Kikunoha/cprg306-assignments</Link></p>
       </main>
     );
 }
